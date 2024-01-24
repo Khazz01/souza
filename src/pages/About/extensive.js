5d379df1936bfb5166e54f92ff19b3fa9c5c1fbe
Kesiena David Okome <117358951+Khazz01@users.noreply.github.com>
2024-01-24 14:08:19 +0100

    Update extensive.js

--- src/pages/About/extensive.js
+++ src/pages/About/extensive.js
@@ -5,7 +5,7 @@ return (
     <div className="about-me-texts">
               <p>As an aspiring product manager, I am dedicated to creating products that seamlessly blend technology with human experiences. I excel in dynamic environments, utilizing agile methodologies to drive innovation and surpass customer expectations. Beyond my professional pursuits, I enjoy exploring Nigeria and engaging in adventurous activities, savoring the finer aspects of life. I take pleasure in sharing new experiences and aim to bring a unique mix of professionalism and zest for life to all endeavors. Eager to participate in mentorship programs and accountability groups, I view myself as a work in progress, striving to balance a promising career with a life filled with passion and exploration. This page serves as a glimpse into my journey, inviting others to join me in discovering something new!</p>
 
-<h2>Product Manager Responsibilities</h2>
+<h2>My <span className="purple">Responsibilities</span></h2>
 <ul>
     <li>I define the product's vision, mission, and goals.</li>
     <li>I conduct market research to identify customer needs and market trends.</li>
